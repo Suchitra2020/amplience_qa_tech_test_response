faad003a31b751c3a262e9af961db946a3b4d761
Suchitra2020 <61415062+Suchitra2020@users.noreply.github.com>
2022-04-15 18:31:00 +0100

    Part 2: Github User Profile UI Test Automation
    Using selenium chrome webdriver with python & pytest framework

--- test/test_git_user_api.py
+++ test/test_git_user_api.py
@@ -1,5 +1,11 @@
 import pytest     
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+## set chromedriver.exe path
+chromedriver_exe_path =  '../chromedriver'
 
 def call_api(param):
     """
@@ -15,6 +21,23 @@ def call_api(param):
     api_url = 'https://api.github.com/users/' + param['user_id']
     api_resp = requests.get(api_url)
     return api_resp
+
+def launch_web(param):
+    """
+    Uses selenium chrome webdriver to launch github profile page for specified user.
+
+    Parameters:
+        param (dict): dict must contain 'user_id' key with a valid git user value.
+    
+    Returns:
+        driver: instance of the webdriver interface
+    """
+    
+    s = Service(chromedriver_exe_path) 
+    driver = webdriver.Chrome(service=s)
+
+    driver.get("https://github.com/" + param['user_id'])
+    return driver
 
 ## API Testing
 
@@ -42,4 +65,23 @@ def test_api_resp_body():
     resp_body = api_response.json() 
     for key,value in expected_resp.items():
         assert resp_body[key] == value,"Test failed: Should be"+value
+    
+def test_git_ui_elements():   
+    resp_body = api_response.json()
+
+    driver = launch_web(test_param)
+    html_elements = {
+        'login': './/span[@class = "p-nickname vcard-username d-block"]',
+        'name': './/span[@class = "p-name vcard-fullname d-block overflow-hidden"]',
+        'location': './/span[@class ="p-label"]',
+        'public_repos' : '//*[@id="js-pjax-container"]/div[1]/div/div/div[2]/div/nav/a[2]/span',
+        'followers': '(.//span[@class = "text-bold color-fg-default"])[1]', 
+        'following': '(.//span[@class = "text-bold color-fg-default"])[2]'
+    }
+
+    for key,value in html_elements.items():
+        for elem in driver.find_elements(By.XPATH, value):
+            assert elem.text == str(resp_body[key])
+    
+    driver.quit()
 
